f344c142bb9b6f67ff6423f5c87fa7eddfc5cd43
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-10-09 01:39:22 +0530

    playing with annotations and adding image to database

--- src/main/java/com/tuts/App.java
+++ src/main/java/com/tuts/App.java
@@ -1,5 +1,9 @@
 package com.tuts;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,7 +15,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Application started.." );
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
@@ -22,11 +26,27 @@ public class App
         st.setName("Deepak Kumar");
         st.setCity("Delhi");
         
+        //creating object for the address
+        Address ad = new Address();
+        ad.setStreet("Alaula");
+        ad.setCity("Kalyanpur");
+        ad.setIsOpen(true);
+        ad.setAddedDate(new Date());
+        //image setup
+        FileInputStream fileInputStream = new FileInputStream("src/main/java/user.jpg");
+        byte[] img_data = new byte[fileInputStream.available()];
+        fileInputStream.read(img_data);
+        ad.setImage(img_data);
+        
         //creating session object
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
         session.save(st);
+        session.save(ad);
         transaction.commit();
-        session.close();        
+        session.close();    
+        System.out.println("All done...");
+        
+        
     }
 }
